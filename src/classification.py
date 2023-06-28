5683142d8c34dc55e482814892b222776d28ebb0
alimustoofaa <softhouse.ali@gmail.com>
2023-06-28 14:41:47 +0700

    add compare image

--- src/classification.py
+++ src/classification.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 import numpy as np
 
 # Config for Detection
@@ -6,6 +7,9 @@ IMAGE_HEIGHT, IMAGE_WIDTH = 416, 416
 DET_MEDIAN_BLUR_1 = 11
 DET_MEDIAN_BLUR_2 = 91
 DET_THRESOLD = 40
+
+IMAGE_FERTILE = cv2.imread('/home/pi/Projects/Egg-Detection/src/blured_fertile.jpg')
+IMAGE_INFERTILE = cv2.imread('/home/pi/Projects/Egg-Detection/src/blured_infertile.jpg')
 
 def image_to_rgb(image):
     '''
@@ -63,6 +67,7 @@ def filter_contour(list_contour):
         Find max value and get on of max value contour
     '''
     len_contour = [len(i) for i in list_contour]
+    print(len_contour)
     max_value = max(len_contour)
     idx = len_contour.index(max_value)
     max_contour = list_contour[idx]
@@ -95,7 +100,7 @@ CLASS_SIG_SPACE = 13
 CLASS_CLIP_LIMIT = 136.0
 CLASS_THRES  = 160
 CLASS_MIN_CONTOUR = 15
-CLASS_MAX_CONTOUR = 90
+CLASS_MAX_CONTOUR = 50
 CLASS_IMAGE_WIDTH = 250
 CLASS_IMAGE_HEIGH = 250
 CLASS_MIN_AREA = 15
@@ -153,10 +158,49 @@ def filter_object_contour(current, image_bg_white, min_contour, max_contour):
             total_area+=1
     return total_area, image_bg_white
 
+def compare_image(image_current):
+    image_infertil=IMAGE_INFERTILE
+    image_fertil=IMAGE_FERTILE
+    image_infertil_g = cv2.cvtColor(image_infertil, cv2.COLOR_BGR2GRAY)
+    image_fertil_g = cv2.cvtColor(image_fertil, cv2.COLOR_BGR2GRAY)
+    image_current_g = cv2.cvtColor(image_current, cv2.COLOR_BGR2GRAY)
 
+    image_infertil_h = cv2.calcHist([image_infertil_g], [0],
+                         None, [256], [0, 256])
+    image_fertil_h = cv2.calcHist([image_fertil_g], [0],
+                         None, [256], [0, 256])
+    image_current_h = cv2.calcHist([image_current_g], [0],
+                         None, [256], [0, 256])
+
+    c1, c2 = 0, 0
+
+    # Euclidean Distance between data1 and test
+    i = 0
+    while i<len(image_current_h) and i<len(image_infertil_h):
+        c1+=(image_current_h[i]-image_infertil_h[i])**2
+        i+= 1
+    c1 = c1**(1 / 2)
+
+
+    # Euclidean Distance between data2 and test
+    i = 0
+    while i<len(image_current_h) and i<len(image_fertil_h):
+        c2+=(image_current_h[i]-image_fertil_h[i])**2
+        i+= 1
+    c2 = c2**(1 / 2)
+
+    if(c1<c2):
+        return 200
+    else:
+        return 150
+    
 def main_process(image=None):
-    image = cv2.imread('/Users/alimustofa/Downloads/telur 3/images (3).jpg')
     h, w  = image.shape[:2]
+    image = image[
+        0:360, 0:w
+    ]
+    cv2.imwrite(f'results/{int(time.time())}.jpg', image)
+    h,w = image.shape[:2]
     # change image to rgb
     image_rgb = image_to_rgb(image)
     # resize image
@@ -221,8 +265,12 @@ def main_process(image=None):
                                     )
     # improve image contrast using object clahe
     image_clahed = object_clahe(image_blured_bil, CLASS_CLIP_LIMIT)
+    # compare image get val thresold
+    image_clahed_com = cv2.cvtColor(image_clahed, cv2.COLOR_GRAY2BGR)
+    thresold_val = compare_image(image_clahed_com)
+    print(f'Value thresold : {thresold_val}')
     # thresolding image to zero
-    image_thres_tozero = image_thresold_tozero(image_clahed, CLASS_THRES)
+    image_thres_tozero = image_thresold_tozero(image_clahed, thresold_val)
     # image morphology
     image_morpho = image_morphology(image_thres_tozero)
     total_area, image_class_drawed = filter_object_contour(
@@ -231,11 +279,15 @@ def main_process(image=None):
                                             CLASS_MIN_CONTOUR, 
                                             CLASS_MAX_CONTOUR
                                         )
-    classification = 'Fertil' if CLASS_MIN_AREA >= 15 else 'Infertil'
-    image_class_drawed = image_resize(image_class_drawed,w_crop, h_crop)
+    print(f'Count area = {total_area}')
+    classification = 'fertil' if total_area >= CLASS_MIN_AREA else 'infertil'
+    image_class_drawed = image_resize(image_class_drawed, w_crop, h_crop)
     # image_class_drawed = draw_image(image_class_drawed, max_contour)
-    cv2.imwrite('crop.jpg', cv2.cvtColor(image_class_drawed, cv2.COLOR_BGR2RGB))
-    print(classification)
+    # cv2.imwrite('crop.jpg', cv2.cvtColor(image_class_drawed, cv2.COLOR_BGR2RGB))
+    # print(classification)
+    image_class_drawed = cv2.cvtColor(image_class_drawed, cv2.COLOR_BGR2RGB)
+    return image_class_drawed, classification
     
 if __name__ == '__main__':
-    main_process()
+    image = cv2.imread('/home/pi/Projects/Egg-Detection/results/1687874167.jpg')
+    print(main_process(image)[1])
